4fbb09c4e6b0c96a317d42e5ca15261abb697ef0
tlepple <49572881+tlepple@users.noreply.github.com>
2023-01-28 10:08:10 -0600

    Update setup_data_origination_apps.sh

--- setup_data_origination_apps.sh
+++ setup_data_origination_apps.sh
@@ -179,7 +179,6 @@ rm -f passwd.txt
 ## Run the sql file to create the schema for all DB’s
 ##########################################################################################
 sudo -u postgres psql < ~/data_origination_workshop/db_ddl/create_user_datagen.sql
-#sudo -u datagen psql < ~/data_origination_workshop/db_ddl/create_ddl_datagen.sql
 sudo -u datagen psql < ~/data_origination_workshop/db_ddl/customer_ddl.sql
 sudo -u datagen psql < ~/data_origination_workshop/db_ddl/customer_function_ddl.sql
 sudo -u datagen psql < ~/data_origination_workshop/db_ddl/grants4dbz.sql
