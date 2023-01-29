36dff651f6ca90f3e6733d58dc7db9d5203490d9
tlepple <49572881+tlepple@users.noreply.github.com>
2023-01-29 07:59:39 -0600

    Update comsume_topic_dgCustomer.py

--- datagen/comsume_topic_dgCustomer.py
+++ datagen/comsume_topic_dgCustomer.py
@@ -1,12 +1,44 @@
 import json 
+import sys
+import argparse
 from kafka import KafkaConsumer
 
-if __name__ == '__main__':
-    # Kafka Consumer 
+startKey = int(1)
+#iterateVal = int(5)
+
+parser = argparse.ArgumentParser()
+
+# define our required arguments to pass in:
+parser.add_argument("recordCount", help="Enter int value for desired number of records", type=int)
+
+# parse these args
+args = parser.parse_args()
+
+# assign args to vars:
+stopVal = int(args.recordCount)
+
+try:
+    # define our Kafka Consumer 
     consumer = KafkaConsumer(
         'dgCustomer',
         bootstrap_servers='localhost:9092',
-        auto_offset_reset='earliest'
+        auto_offset_reset='earliest',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
     )
     for message in consumer:
-        print(json.loads(message.value))
+        print(message.value)
+        startKey += 1 
+
+        if startKey == stopVal:
+            print("\n")
+            print(str(stopVal) +  " msgs have been consumed.")
+            print("\n")
+            consumer.close()
+            sys.exit()
+
+
+except KeyboardInterrupt:
+    sys.exit()
+finally:
+    print("script complete!")
+
