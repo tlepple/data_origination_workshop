22b17201decf541445cbc0ed85c634b7282fce6e
tlepple <49572881+tlepple@users.noreply.github.com>
2023-01-29 08:11:03 -0600

    Update test_pg.py

--- datagen/test_pg.py
+++ datagen/test_pg.py
@@ -1,16 +1,43 @@
 import psycopg2
 
-# Connect to your PostgreSQL database on a remote server
-conn = psycopg2.connect(host="127.0.0.1", port="5432", dbname="datagen", user="datagen", password="supersecret1")
+try:
+    try:
+         # Connect to your PostgreSQL database on a remote server
+         conn = psycopg2.connect(host="127.0.0.1", port="5432", dbname="datagen", user="datagen", password="supersecret1")
+         print("Connection Established!")
+         print("\n")
+    except psycopg2.Error as exception:
+         printf ('Failed to connect to database')
+         printException (exception)
+         exit (1)
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    try:
+        # Execute a test query
+        cur.execute("SELECT * FROM customer")
 
-# Execute a test query
-cur.execute("SELECT * FROM customer")
+        # Retrieve query results
+        records = cur.fetchall()
 
-# Retrieve query results
-records = cur.fetchall()
+        #print records
+        print(records)
 
-# Finally, you may print the output to the console or use it anyway you like
-print(records)
+    except psycopg2.Error as exception:
+        printf ('Failed to insert\n')
+        printException (exception)
+        exit (1)
+    finally:
+        if(conn):
+            cur.close()
+            conn.close()
+            print("\n")
+            print("PostgreSQL connection is closed")
+
+except (Exception, psycopg2.Error) as error:
+    print("Something else went wrong...\n", error)
+
+finally:
+    print("\n")
+    print("script complete!")
+
