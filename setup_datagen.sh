4928428525d77f33262d0dcc8e386d2ab1f71922
tlepple <49572881+tlepple@users.noreply.github.com>
2023-02-16 19:06:06 -0600

    Update setup_datagen.sh

--- setup_datagen.sh
+++ setup_datagen.sh
@@ -94,6 +94,10 @@ EOF
 
 sudo cp ~/data_origination_workshop/redpanda/redpanda.yaml /etc/redpanda/
 
+
+##########################################################################################
+#  Need to update the value of '<private_ip>' in a bunch of files
+##########################################################################################
 PRIVATE_IP=`ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'`
 sudo sed -e "s,<private_ip>,$PRIVATE_IP,g" -i ~/redpanda-console-config.yaml
 sudo sed -e "s,<private_ip>,$PRIVATE_IP,g" -i /etc/redpanda/redpanda.yaml
