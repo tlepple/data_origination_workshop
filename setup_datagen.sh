37ed10d0ecec8e8d3823d025d6e608d1b2d08741
tlepple <49572881+tlepple@users.noreply.github.com>
2023-02-18 12:40:51 -0600

    Update setup_datagen.sh

--- setup_datagen.sh
+++ setup_datagen.sh
@@ -337,8 +337,8 @@ wget https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 
 tar -xzvf spark-3.3.1-bin-hadoop3.tgz
 
-#sudo mv spark-3.3.1-bin-hadoop3/ /opt/spark
-mv spark-3.3.1-bin-hadoop3/ /opt/spark
+sudo mv spark-3.3.1-bin-hadoop3/ /opt/spark
+#mv spark-3.3.1-bin-hadoop3/ /opt/spark
 
 ##########################################################################################
 #  install aws cli
