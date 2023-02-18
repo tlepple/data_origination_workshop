cfd3864e23f5e126547c43ed9ff427d39594d3a8
tlepple <49572881+tlepple@users.noreply.github.com>
2023-02-18 11:39:48 -0600

    Create iceberg_workshop_tbl_ddl.sql

---
+++ spark_items/iceberg_workshop_tbl_ddl.sql
@@ -0,0 +1,31 @@
+CREATE TABLE icecatalog.icecatalog.customer (
+    first_name STRING,
+    last_name STRING,
+    street_address STRING,
+    city STRING,
+    state STRING,
+    zip_code STRING,
+    home_phone STRING,
+    mobile STRING,
+    email STRING,
+    ssn STRING,
+    job_title STRING,
+    create_date STRING,
+    cust_id BIGINT)
+USING iceberg
+OPTIONS (
+    'write.object-storage.enabled'=true,
+    'write.data.path'='s3://iceberg-data');
+
+CREATE TABLE icecatalog.icecatalog.transactions (
+    transact_id STRING,
+    transaction_date STRING,
+    item_desc STRING,
+    barcode STRING,
+    category STRING,
+    amount STRING,
+    cust_id BIGINT)
+USING iceberg
+OPTIONS (
+    'write.object-storage.enabled'=true,
+    'write.data.path'='s3://iceberg-data');
