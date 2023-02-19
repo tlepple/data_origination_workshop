4a9237f71a88d2dfcf4280a1dfda4c68b9f6700e
tlepple <49572881+tlepple@users.noreply.github.com>
2023-02-19 11:15:41 -0600

    Update setup_datagen2.sh

--- setup_datagen2.sh
+++ setup_datagen2.sh
@@ -168,7 +168,7 @@ apt policy postgresql
 ##########################################################################################
 #curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 
-sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg ~/downloads/$POSTGRESQL_KEY_FILE 
+sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg ~/data_origination_workshop/downloads/$POSTGRESQL_KEY_FILE 
 
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
