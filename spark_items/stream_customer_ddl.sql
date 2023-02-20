3f77bae10f234981f3ad2f149a7c98a2df822e9e
tlepple <49572881+tlepple@users.noreply.github.com>
2023-02-20 09:30:22 -0600

    Update stream_customer_ddl.sql

--- spark_items/stream_customer_ddl.sql
+++ spark_items/stream_customer_ddl.sql
@@ -1,1 +1,20 @@
 
+
+CREATE TABLE icecatalog.icecatalog.stream_customer_event_history (
+    first_name STRING,
+    last_name STRING,
+    street_address STRING,
+    city STRING,
+    state STRING,
+    zip_code STRING,
+    home_phone STRING,
+    mobile STRING,
+    email STRING,
+    ssn STRING,
+    job_title STRING,
+    create_date STRING,
+    cust_id BIGINT)
+USING iceberg
+OPTIONS (
+    'write.object-storage.enabled'=true,
+    'write.data.path'='s3://iceberg-data');
